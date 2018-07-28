8e810575ea6b50944e7d3e7a72098b46e89fdd49
wktdev <wktdev@gmail.com>
2018-07-27 23:53:54 -0700

    error fix in progress, clean uo console.logs

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Stop from './components/Stop';
 import StateRender from './components/StateRender';
 
 
-
 import {createStore} from 'redux';
 import {Provider} from "react-redux";
 import combinedReducers from "./reducers"
@@ -14,7 +13,6 @@ const store = createStore(combinedReducers, window.__REDUX_DEVTOOLS_EXTENSION__ 
 class App extends Component {
   
   render() {
-    console.log(store);
     return (
       <Provider store={store}>
           <div className="App">
